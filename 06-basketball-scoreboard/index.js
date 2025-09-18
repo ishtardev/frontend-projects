04e6b602a06b19df27621d8f429b8aef08615d5b
ishtar <secretgoddess6666@gmail.com>
2025-09-18 18:03:00 +0100

    updated the code

---
+++ 06-basketball-scoreboard/index.js
@@ -0,0 +1,35 @@
+let home = document.getElementById("home")
+let guest = document.getElementById("guest")
+
+let count = 0
+let countguest = 0
+
+function add1() {
+    count += 1
+    home.textContent = count
+}
+
+function add2() {
+    count += 2
+    home.textContent = count
+}
+
+function add3() {
+    count += 3
+    home.textContent = count
+}
+
+function guestadd1() {
+    countguest += 1
+    guest.textContent = count
+}
+
+function guestadd2() {
+    countguest += 2
+    guest.textContent = count
+}
+
+function guestadd3() {
+    countguest += 3
+    guest.textContent = count
+}
