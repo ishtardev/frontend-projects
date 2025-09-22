ace5ff41464f2fa3ce84503f7506db79f67c89ec
ishtar <secretgoddess6666@gmail.com>
2025-09-22 14:52:36 +0100

    update code

--- 08-chrome-extension/index.js
+++ 08-chrome-extension/index.js
@@ -1,8 +1,11 @@
-let tabs = []
 let inputEl = document.getElementById("input-el")
 let saveinput = document.getElementById("save")
+let saveTab = document.getElementById("tab")
+
+let ulEl = document.getElementById("ul-el")
 
 saveinput.addEventListener("click", function(){
-    tabs.push(inputEl.value)
-    console.log("tabs", tabs)
+    ulEl.innerHTML += `<li><a href="#">${inputEl.value}</a></li> `
 })
+
+
