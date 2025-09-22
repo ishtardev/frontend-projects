584539b79e75a9b456392b523776bd16d1d9bf11
ishtar <secretgoddess6666@gmail.com>
2025-09-22 17:06:31 +0100

    implemented localStorage for tabs

--- 08-chrome-extension/index.js
+++ 08-chrome-extension/index.js
@@ -1,11 +1,27 @@
+let tabs = []
+
 let inputEl = document.getElementById("input-el")
 let saveinput = document.getElementById("save")
 let saveTab = document.getElementById("tab")
 
 let ulEl = document.getElementById("ul-el")
 
+let tabsFromLocalStorage = JSON.parse(localStorage.getItem("tabs"))
+if (tabsFromLocalStorage ){
+    tabs = tabsFromLocalStorage 
+    render()
+}
+
 saveinput.addEventListener("click", function(){
-    ulEl.innerHTML += `<li><a href="#">${inputEl.value}</a></li> `
+    tabs.push(inputEl.value)
+    render()
+    
+    localStorage.setItem("tabs",JSON.stringify(tabs))
+
+    console.log(localStorage.getItem("tab"))
 })
 
+function render() {
+    ulEl.innerHTML += `<li><a href="#">${inputEl.value}</a></li> `
+}
 
