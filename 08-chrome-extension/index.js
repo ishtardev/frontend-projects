5280aecb5a376682c18f85ca7f8aa380914a32b1
ishtar <secretgoddess6666@gmail.com>
2025-09-22 17:27:30 +0100

    add delete button

--- 08-chrome-extension/index.js
+++ 08-chrome-extension/index.js
@@ -3,25 +3,38 @@ let tabs = []
 let inputEl = document.getElementById("input-el")
 let saveinput = document.getElementById("save")
 let saveTab = document.getElementById("tab")
+let deleteBtn = document.getElementById("delete")
 
 let ulEl = document.getElementById("ul-el")
 
+deleteBtn.addEventListener("click", function() {
+    localStorage.clear()
+    tabs = []
+    render(tabs)
+})
+
 let tabsFromLocalStorage = JSON.parse(localStorage.getItem("tabs"))
+
 if (tabsFromLocalStorage ){
     tabs = tabsFromLocalStorage 
-    render()
+    render(tabs)
 }
 
 saveinput.addEventListener("click", function(){
     tabs.push(inputEl.value)
-    render()
-    
+    render(tabs)
+
     localStorage.setItem("tabs",JSON.stringify(tabs))
 
     console.log(localStorage.getItem("tab"))
 })
 
-function render() {
-    ulEl.innerHTML += `<li><a href="#">${inputEl.value}</a></li> `
+function render(arr) {
+    let itms = ""
+    for (let i = 0; i < arr.length; i++){
+        itms += "<li><a href='#'>"+arr[i]+"</a></li>"
+    }
+    ulEl.innerHTML = itms
 }
+
 
